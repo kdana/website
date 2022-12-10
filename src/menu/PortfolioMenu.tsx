cfd86c76c5c7c7940ff61a63e2a162282b39cc0c
Karen <kdana.dev@gmail.com>
2022-12-09 19:28:32 -0500

    Fix desktop menu spacing

--- src/menu/PortfolioMenu.tsx
+++ src/menu/PortfolioMenu.tsx
@@ -39,7 +39,7 @@ function PortfolioMenu() {
           >
             <MenuIcon />
           </IconButton>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", sm: "flex" } }}></Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", sm: "none" } }}></Box>
           <SwipeableDrawer
             id="menu-appbar"
             anchor="left"
